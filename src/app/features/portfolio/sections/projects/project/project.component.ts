60e47caaa5ff1cb1bd907795d5353a317dcc2732
Simon Heistermann <simon.heistermann@icloud.com>
2025-05-06 19:31:33 +0200

    fixed s few bugs and added project component

---
+++ src/app/features/portfolio/sections/projects/project/project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  imports: [],
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.sass'
+})
+export class ProjectComponent {
+
+}
